33ac76c036cb56df096480d24b74599defc54428
Mayya Abzhigitova <mayya.abzhigitova@gmail.com>
2019-12-05 14:50:03 -0800

    Class2

---
+++ Classes/Classes/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    //this class is a blue print of a datatype
+    class Human
+    {
+
+        //member variable
+        public string firstName;
+        public string lastName;
+
+        //member
+        public void IntroduceMySelf()
+        {
+            Console.WriteLine("My name is {0}", firstName +" "+ lastName) ;
+        }
+
+
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of my class
+            //an unstance of human
+            Human mayya = new Human();
+            //access public variables from outside and even change it
+            mayya.firstName = "Mayya";
+            mayya.lastName = "Abzhigitova";
+            //call method of a class
+            mayya.IntroduceMySelf();
+
+            
+            Console.ReadKey();
+        }
+    }
+}
