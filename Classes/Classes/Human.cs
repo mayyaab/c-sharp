b2bba75231a32ec43d229189e946e2d44e631a2c
Mayya Abzhigitova <mayya.abzhigitova@gmail.com>
2019-12-05 17:52:52 -0800

    Class3

--- Classes/Classes/Human.cs
+++ Classes/Classes/Human.cs
@@ -9,13 +9,31 @@ namespace Classes
     {
 
         //member variable
-        public string firstName;
-        public string lastName;
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private int age;
+
+        //constructor
+        public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
 
         //member
         public void IntroduceMySelf()
         {
-            Console.WriteLine("My name is {0}", firstName +" "+ lastName) ;
+            if (age == 1)
+            {
+                Console.WriteLine("My name is {0} {1} and my color of hairs is {2} and year {3} old", firstName, lastName, eyeColor, age);
+            }
+            else
+            {
+                Console.WriteLine("My name is {0} {1} and my color of hairs is {2} and years {3} old", firstName, lastName, eyeColor, age);
+            }
         }
 
 
--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -8,12 +8,18 @@ namespace Classes
         {
             //create an object of my class
             //an unstance of human
-            Human mayya = new Human();
+            Human mayya = new Human("Mayya", "Abzhigitova", "browne", 24);
+            Human tigran = new Human("Tigram", "Jan", "browne", 100);
+            Human mia = new Human("Mia", "Kim", "blue", 1);
             //access public variables from outside and even change it
-            mayya.firstName = "Mayya";
-            mayya.lastName = "Abzhigitova";
+            //mayya.firstName = "Mayya";
+            //mayya.lastName = "Abzhigitova";
             //call method of a class
             mayya.IntroduceMySelf();
+            tigran.IntroduceMySelf();
+            mia.IntroduceMySelf();
+
+
 
             
             Console.ReadKey();
