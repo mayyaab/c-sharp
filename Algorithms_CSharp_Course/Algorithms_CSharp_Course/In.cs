4f75c09267a1589e16394f1c44030ee44936785e
Mayya Abzhigitova <mayya@pango.co>
2020-01-13 21:29:06 -0800

    Big file has been removed
    Algorithms_start

---
+++ Algorithms_CSharp_Course/Algorithms_CSharp_Course/In.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Algorithms_CSharp_Course
+{
+    class In
+    {
+        public static IEnumerable<int> ReadInts(string filePath)
+        //IEnumerable - интерфйес
+        {
+            using (TextReader reader = File.OpenText(filePath))
+            {
+                string lastLine;
+                while ((lastLine = reader.ReadLine()) != null)
+                {
+                    yield return int.Parse(lastLine);
+                }
+            }
+        }
+
+    }
+}
---
+++ Algorithms_CSharp_Course/Algorithms_CSharp_Course/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Algorithms_DataStruct_Lib;
+
+namespace Algorithms_CSharp_Course
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            //300 milisecons for 1k
+            //14 second for 4k
+            //1 min 36 sec for 8k 
+            var ints = In.ReadInts("4Kints.txt").ToArray();
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var triplets = ThreeSum.Count(ints);
+
+            watch.Stop();
+
+            Console.WriteLine($"The number of \"zero-sum\" triplets: {triplets}");
+            Console.WriteLine($"Time taken: {watch.Elapsed:g}");
+
+            Console.Read();
+        }
+    }
+}
