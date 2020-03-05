95851b4f2de9741e400dde69fb0034c2ef56afac
Mayya Abzhigitova <mayya@pango.co>
2020-03-05 14:07:06 -0800

    Inheritance

---
+++ Inheritance/Inheritance/ImagePost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+
+    //ImagePost derives from Post and adds a property (ImageURL) and two constructors
+    class ImagePost: Post
+    {
+        public string ImageURL { get; set; }
+
+        public ImagePost() { }
+
+        public ImagePost ( string title, string sendByUsername, string imageUrl, bool isPublic)
+        {
+            //The following properties and the GetNextID method are inherited from Post
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+
+            //Property ImageUrl is a member of ImagePost, but not of Post
+            this.ImageURL = imageUrl;
+        }
+
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post("Thank for the b-day wishes", true, "Maya A");
+            Console.WriteLine(post1.ToString());
+            Console.ReadLine();
+
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Mayya", "https://image.com", true);
+
+            Console.WriteLine(imagePost1.ToString());
+            Console.ReadLine();
+        }
+    }
+}
