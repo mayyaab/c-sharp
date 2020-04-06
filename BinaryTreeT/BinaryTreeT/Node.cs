d70522e382f5fda7e0a8f0723c6df69cd6011952
Mayya Abzhigitova <mayya@pango.co>
2020-04-05 21:14:42 -0700

    BinaryTree+Write a program that finds in a binary tree of numbers the sum of the vertices of each level of the tree.

---
+++ BinaryTreeT/BinaryTreeT/Node.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeT
+{
+    public class BinaryTree<T>
+
+    {
+        public T Value { get; set; }
+        public BinaryTree<T> LeftChild { get;  set; }
+        public BinaryTree<T> RightChild { get;  set; }
+
+        public BinaryTree(T value,
+            BinaryTree<T> leftChild, BinaryTree<T> rightChild)
+        {
+            this.Value = value;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
+        }
+
+        public BinaryTree(T value) : this(value, null, null)
+        {
+
+        }
+
+        //Traverses the binary tree in pre-order
+        public void PrintInOrder()
+        {
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.PrintInOrder();
+            }
+            Console.Write(this.Value + " ");
+            if (this.RightChild != null)
+            {
+                this.RightChild.PrintInOrder();
+            }
+        }
+
+        //DFS
+        public void PrintDFS()
+        {
+            Console.Write(this.Value + " ");
+
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.PrintDFS();
+            }
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.PrintDFS();
+            }
+        }
+
+        //Write a program that finds in a binary tree of numbers the sum of the vertices of each level of the tree.
+        
+
+
+
+
+        
+
+        
+    }
+}
---
+++ BinaryTreeT/BinaryTreeT/Tree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeT
+{
+    public class Tree
+    {
+        public BinaryTree<int> _root;
+
+        public static List<int> VerticesSumByLevel(BinaryTree<int> root, List<int> list = null, int level = 0)
+        {
+            if (list == null)
+            {
+                list = new List<int>();
+            }
+
+            if (level == list.Count)
+            {
+                list.Add(root.Value);
+            }
+
+            else
+            {
+                list[level] += root.Value;
+            }
+
+            if (root.LeftChild != null)
+            {
+                VerticesSumByLevel(root.LeftChild, list, level + 1);
+            }
+            
+
+            if (root.RightChild != null)
+            {
+                VerticesSumByLevel(root.RightChild, list, level + 1);
+            }
+            return list;
+        }
+
+       
+}
+}
