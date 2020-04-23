32d146c82ba6f711b50009ecbe0e573cd3ea6a69
Mayya Abzhigitova <mayya@pango.co>
2020-04-23 15:53:41 -0700

    Game

---
+++ Game/Game/Graph.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    public class Graph
+    {
+        private const int Height = 6;
+        private const int Width = 6;
+
+
+        public Graph()
+        {
+        }
+
+        public static void RandomizeArray()
+        {
+            int[,] array = new int[Height, Width];
+            Random random = new Random();
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    array[i, j] = random.Next(4);
+
+                    Console.Write("{0,4}", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
