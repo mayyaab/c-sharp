6bf8d5f27f2849ca3d008765cff90cd9ad80e6fd
Tigran <tigran@anchorfree.com>
2020-04-24 19:50:42 -0700

    suggestions

--- Game/Game/Graph.cs
+++ Game/Game/Graph.cs
@@ -5,16 +5,23 @@ using System.Text;
 
 namespace Game
 {
+    // Rename to Field
     public class Graph
     {
         private const int Height = 6;
         private const int Width = 6;
 
+        // Have a array as a member variable
 
+        // Make constructor accepting Height & Width
         public Graph()
         {
         }
 
+        // Add function PlaceBalls that will place 3 balls on a field at random positions
+        // PlaceBalls()
+
+        // Modify to a function "Print" which prints the field
         public static void RandomizeArray()
         {
             int[,] array = new int[Height, Width];
--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -6,7 +6,12 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            Graph.RandomizeArray();
+            // get user input in a loop here
+            // it should handle different commands
+            // "show" command should print the field
+
+
+            // Graph.RandomizeArray();
         }
     }
 }
