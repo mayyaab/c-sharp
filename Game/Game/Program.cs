2df52cb64c4c164ad80b16a85087e8c43604c576
Tigran <tigran@anchorfree.com>
2020-04-24 19:54:41 -0700

    hint

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -9,7 +9,7 @@ namespace Game
             // get user input in a loop here
             // it should handle different commands
             // "show" command should print the field
-
+            // Some useful example https://www.dotnetperls.com/console-readline
 
             // Graph.RandomizeArray();
         }
